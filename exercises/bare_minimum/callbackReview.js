518f33067fcb3695e5d237615b421e874b30e8e1
mille424 <mille424@gmail.com>
2019-06-17 19:44:27 -0700

    callbackReview complete

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,10 +4,30 @@
 
 var fs = require('fs');
 var request = require('request');
+const readLine = require('readline');
+
+
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  // TODO 
+
+  fs.readFile(filePath, 'utf8', function(err, data) {
+    if (err) {
+      callback(err);
+    } else if (data) {
+      var rl = readLine.createInterface({input:fs.createReadStream(filePath)})
+      var counter = 0;
+      let firstLine = '';
+      rl.on('line', function(lineVal) {
+        if (counter === 0 ) {
+          firstLine = lineVal;
+          callback(null, firstLine);
+        } 
+        counter++;
+      });   
+    }
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
