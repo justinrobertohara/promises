3b03d648cf6e62aad048bf0a91b46eab4460dcdf
mille424 <mille424@gmail.com>
2019-06-19 21:07:40 -0700

    working on final promise test

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,13 +10,30 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
-
+var pluckFirstLineFromFileAsync = require('./promiseConstructor.js');
+var https = require('https');
+var request = require('request');
+var wf = Promise.promisify(fs.writeFile);
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
-};
+  return pluckFirstLineFromFileAsync.pluckFirstLineFromFileAsync(readFilePath)
+    .then(function(username) {
+      request(`https://api.github.com/users/${username}`, function(error, response, body) {
+      });
+    //var url = 'https://api.github.com/users/' + userName
+    })
 
+    //ENDING HERE
+    //WE ARE GETTING TO SECOND THEN
+    //githubUserProfile is currently undefined
+    .then(function(githubUserProfile) {
+      console.log('we made it to second then');
+      console.log(githubUserProfile);
+      return wf(writeFilePath, githubUserProfile);
+    })
+    .catch(error => console.log(error));
+};
 // Export these functions so we can test them
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
