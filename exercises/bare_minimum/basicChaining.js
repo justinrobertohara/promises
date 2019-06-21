b33fabac5835ecd01c78019456f2acb9b7bd545d
mille424 <mille424@gmail.com>
2019-06-20 19:13:55 -0700

    Complete Bare Minimum Requirements

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,27 +10,21 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-var pluckFirstLineFromFileAsync = require('./promiseConstructor.js');
+var promiseConstructor = require('./promiseConstructor.js');
 var https = require('https');
 var request = require('request');
-var wf = Promise.promisify(fs.writeFile);
+var writeFilePromise = Promise.promisify(fs.writeFile);
+var requestPromise = Promise.promisify(request);
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
-  return pluckFirstLineFromFileAsync.pluckFirstLineFromFileAsync(readFilePath)
-    .then(function(username) {
-      request(`https://api.github.com/users/${username}`, function(error, response, body) {
-      });
-    //var url = 'https://api.github.com/users/' + userName
+  return promiseConstructor.pluckFirstLineFromFileAsync(readFilePath)
+    .then(function(username) {    
+      return requestPromise(`https://api.github.com/users/${username}`);  
     })
-
-    //ENDING HERE
-    //WE ARE GETTING TO SECOND THEN
-    //githubUserProfile is currently undefined
     .then(function(githubUserProfile) {
-      console.log('we made it to second then');
-      console.log(githubUserProfile);
-      return wf(writeFilePath, githubUserProfile);
+      //console.log('the body of githubUserProfile: ', githubUserProfile.body);
+      return writeFilePromise(writeFilePath, githubUserProfile.body);
     })
     .catch(error => console.log(error));
 };
